d0ebb4f95e09ae8d3dba05548d0980b36d779fef
Pinguinpanic <rojo_bobby@hotmail.com>
2015-12-06 14:04:29 +0100

    Actually added KeyHandler

---
+++ js/KeyHandler.js
@@ -0,0 +1,63 @@
+keysDeactivated=false;
+
+function KeyHandler()
+{
+    document.addEventListener("keydown",this.keyDownHandler.bind(this),false);
+    document.addEventListener("keyup",this.keyUpHandler.bind(this),false);
+
+
+    this.keys={};
+    this.keys[0]=false;
+    this.lastKey=null;
+}
+
+KeyHandler.instance=null;
+
+KeyHandler.getInstance=function()
+{
+    if(KeyHandler.instance==null)
+    {
+        KeyHandler.instance=new KeyHandler();
+    }
+    return KeyHandler.instance;
+};
+
+KeyHandler.prototype.keyDownHandler = function(event)
+{
+    if(keysDeactivated)
+    {
+        return;
+    }
+    this.keys[event.keyCode]=true;
+    this.lastKey=event.keyCode;
+};
+
+KeyHandler.prototype.keyUpHandler = function(event)
+{
+    this.keys[event.keyCode]=false;
+};
+KeyHandler.prototype.isPressed = function(keyCode)
+{
+    if(this.keys[keyCode]!=null)
+    {
+        return this.keys[keyCode];
+    }
+    return false;
+};
+
+KeyHandler.prototype.anyPressed = function()
+{
+    for(var i in this.keys)
+    {
+        if(this.keys[i])
+        {
+            return true;
+        }
+    }
+    return false;
+};
+
+//Disable spacebar.
+window.onkeydown = function(e) {
+    return !(e.keyCode == 32);
+};

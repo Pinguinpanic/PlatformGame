4cd59fba709c8358c14b51d05a712fc68789a5b5
Pinguinpanic <rojo_bobby@hotmail.com>
2015-12-06 14:04:05 +0100

    Added KeyHandler and Guy controls

--- js/Guy.js
+++ js/Guy.js
@@ -9,11 +9,14 @@ Guy = function (x,y)
     PIXI.Sprite.call(this);
     this.x=x;
     this.y=y;
+    this.hspeed=0.0;
+    this.vspeed=0.0;
 
     this.texture = PIXI.Texture.fromFrame("guy.png");
 };
 Guy.constructor = Guy;
 Guy.prototype = Object.create(PIXI.Sprite.prototype);
+
 
 /**
  * The Guy's update loop.
@@ -21,5 +24,15 @@ Guy.prototype = Object.create(PIXI.Sprite.prototype);
  */
 Guy.prototype.update = function(mult)
 {
-    this.x+=10*mult;
+    if(KeyHandler.getInstance().isPressed(68))
+    {
+        this.hspeed=4
+    }
+    if(KeyHandler.getInstance().isPressed(65))
+    {
+        this.hspeed=-4;
+    }
+    this.hspeed*=Math.pow(.5,mult);
+    this.x+=this.hspeed;
+    this.y+=this.vspeed;
 };

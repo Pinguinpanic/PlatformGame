9e3021aafedb3e12f8969c9e66e60eb0bfdc1be8
Maarten Slenter <maartenslenter@gmail.com>
2015-12-06 15:49:10 +0100

    Added collision checking with the map

--- js/Guy.js
+++ js/Guy.js
@@ -9,6 +9,7 @@ Guy = function (x,y)
     PIXI.Sprite.call(this);
     this.x=x;
     this.y=y;
+    
     this.hspeed=0.0;
     this.vspeed=0.0;
 
@@ -47,9 +48,41 @@ Guy.prototype.update = function(mult)
     //Gravity
     this.vspeed+=mult*500;
 
-    //Actual movement
-    this.x+=this.hspeed*mult;
-    this.y+=this.vspeed*mult;
+    if(!this.checkInWall(this.x+this.hspeed * mult,this.y))
+    {
+            this.x+=this.hspeed * mult;
+    }
+    else
+    {
+            var step;
+            var steps;
+            step=sign(this.hspeed);
+            steps=0;
+            while(!this.checkInWall(this.x+steps,this.y))
+            {
+                    steps+=step;
+            }
+            this.x+=steps-step;
+            this.hspeed=0;
+    }
+    
+    if(!this.checkInWall(this.x,this.y + this.vspeed * mult))
+    {
+            this.y+=this.vspeed * mult;
+    }
+    else
+    {
+            var step;
+            var steps;
+            step=sign(this.vspeed);
+            steps=0;
+            while(!this.checkInWall(this.x,this.y+steps))
+            {
+                    steps+=step;
+            }
+            this.y+=steps-step;
+            this.vspeed=0;
+    }
 
     if(this.y>400)
     {
@@ -57,3 +90,11 @@ Guy.prototype.update = function(mult)
         this.vspeed=0;
     }
 };
+
+Guy.prototype.checkInWall = function(x, y)
+{
+    return main.currentMap.pixelInWall(x - this.width/2, y) 
+            || main.currentMap.pixelInWall(x + this.width/2, y) 
+            || main.currentMap.pixelInWall(x, y)
+            || main.currentMap.pixelInWall(x, y - this.height);
+}
